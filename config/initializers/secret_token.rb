81c706a78e214d6cae036bb4a687e73f2c7687bf
Robby Ranshous <rranshous@gmail.com>
2014-03-17 17:14:45 -0400

    vanilla rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mapr::Application.config.secret_key_base = '85c030d35f92c01c63010ecc16b7c3e640c4f4bdc600926453875a459c23aa613075764e40c940d1cc90476d6121123535d85098eaf2adf0c24fe43e24987e47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mapr::Application.config.session_store :cookie_store, key: '_mapr_session'
