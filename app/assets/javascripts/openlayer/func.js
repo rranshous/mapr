bc0fd0c1c76386b4f260eb6ce2f44714183b727a
Robby Ranshous <rranshous@gmail.com>
2014-04-08 19:53:50 -0400

    got terrain showing on map

--- app/assets/javascripts/openlayer/func.js
+++ app/assets/javascripts/openlayer/func.js
@@ -2,7 +2,25 @@ var map;
 function init() {
   map = new OpenLayers.Map('map');
   var base = new OpenLayers.Layer.OSM()
-  map.addLayers([base]);
+
+  var physical = new OpenLayers.Layer.Google(
+    "Google Physical",
+    {type: google.maps.MapTypeId.TERRAIN}
+  );
+  var street = new OpenLayers.Layer.Google(
+    "Google Streets", // the default
+    {numZoomLevels: 20}
+  );
+  var hybrid = new OpenLayers.Layer.Google(
+    "Google Hybrid",
+    {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 20}
+  );
+  var satellite = new OpenLayers.Layer.Google(
+    "Google Satellite",
+    {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22}
+  );
+
+  map.addLayers([physical]);
   //map.setCenter( new OpenLayers.LonLat(-122.19058, 46.19976), 5);
   map.setCenter( new OpenLayers.LonLat(-13602148.699803, 5812470.6443611), 5);
   $('#submit').click(function() {
